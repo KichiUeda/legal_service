f5614ef9bbc788da931e173de807fba7e02b3dcf
nightlaro <mickocabacungan@gmail.com>
2020-07-16 20:43:56 -0700

    added some app.use middlewares

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 const port = process.env.PORT;
 app.listen(process.env.PORT, () => {
   console.log(`Server is Live! Listening on PORT: ${process.env.PORT}`);
