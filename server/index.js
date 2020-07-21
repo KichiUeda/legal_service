9d260def580eeb7fb28edc9bc60807b4e458f0c4
nightlaro <mickocabacungan@gmail.com>
2020-07-20 19:51:08 -0700

    implemented client request based on product Id

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const pg = require('../database/model.js');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
