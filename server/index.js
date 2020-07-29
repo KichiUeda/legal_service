19c308700f642b5d87d802259b531d02082741f4
nightlaro <mickocabacungan@gmail.com>
2020-07-28 19:04:41 -0700

    changes in server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env');
+});
 const express = require('express');
 const pg = require('../database/model.js');
 const cors = require('cors');
